4d7fd3135a65fa55068bdd1c012f6308d70e470d
Meg <mpenaganti12@gmail.com>
2023-07-27 21:05:56 +0100

    third test

--- src/main/java/com/tdd/findhighestnumber/HighestNumberFinder.java
+++ src/main/java/com/tdd/findhighestnumber/HighestNumberFinder.java
@@ -4,7 +4,17 @@ public class HighestNumberFinder {
 
 
     public int findHighestNumber(int[] array) {
+        int result = 0;
+        if(array.length == 1) {
+            result = array[0];
+        } else {
+            if(array[0] > array[1]) {
+                result = array[0];
+            } else {
+                result = array[1];
+            }
+        }
 
-        return array[0];
+        return result;
     }
 }
